3c89948f1aef4cd0e1c45fa965d57082044f670e
Mishkady <jessdanielova@gmail.com>
2023-02-03 16:57:16 +1100

    daily forecast js

--- src/app.js
+++ src/app.js
@@ -22,7 +22,8 @@
     return `${day}, ${hours}:${minutes}`;
   }
 
-  function displayForecast() {
+  function displayForecast(response) {
+    console.log(response.data.daily);
     let forecastElement = document.querySelector("#forecast");
   
     let days = ["Thu", "Fri", "Sat", "Sun"];
@@ -52,6 +53,13 @@
     console.log(forecastHTML);
   }
   
+  function getForecast (coordinates) {
+    console.log(coordinates);
+    let apiKey = "6782253072f7d90462731a624097fc54";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units= materic`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(displayForecast);
+  }
   
   function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
@@ -74,6 +82,8 @@
         "src",
         `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", response.data.weather[0].description);
+
+    getForecast(response.data.coord)
   }
 
   function search(city) {
@@ -119,4 +129,4 @@
     celsiusLink.addEventListener("click", displayCelsiusTemperature);
     
     search("Melbourne");
-    displayForecast();
+    
